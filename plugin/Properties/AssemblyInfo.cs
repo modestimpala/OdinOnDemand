bffe75fadd677459e8891131311d9d8d99508fe3
ModestImpala <jesse.walker503@gmail.com>
2024-02-15 19:04:01 -0800

    versioning

--- plugin/Properties/AssemblyInfo.cs
+++ plugin/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.94")]
-[assembly: AssemblyFileVersion("0.9.94")]
+[assembly: AssemblyVersion("0.9.95")]
+[assembly: AssemblyFileVersion("0.9.95")]
